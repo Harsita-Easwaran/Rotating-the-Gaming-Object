f4d370fa2206ed216e012ab6427bff5830aef2c2
Harsita Easwaran <atisrah713@gmail.com>
2025-08-20 14:04:32 +0530

    Add program output screenshot

---
+++ Assets/EXP1.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(Vector3.left,Vector3.up,40*Time.deltaTime);
+
+    }
+}
